5feb37bc528130ae4a459063a0220f636ee498b9
Reyan Shah <reyanshah10c@gmail.com>
2025-09-06 16:57:00 +0530

    fixed more issues

--- client/src/pages/cart.tsx
+++ client/src/pages/cart.tsx
@@ -19,7 +19,7 @@ export default function Cart() {
   const { data: cartData, isLoading } = useQuery({
     queryKey: ["/api/cart"],
   });
-
+  
   const checkoutMutation = useMutation({
     mutationFn: async () => {
       await apiRequest("POST", "/api/orders", {});
